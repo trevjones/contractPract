9021a51556f106233c279975c7dde2ca2f3e6b93
Trevor Jones <38113711+trevjones@users.noreply.github.com>
2018-09-10 12:44:16 -0700

    Update timeCapsule.sol

--- timeCapsule.sol
+++ timeCapsule.sol
@@ -15,3 +15,23 @@ contract timeCapsule {
     }
 
 }
+
+/******ORIGINAL SNIPPET FUNCTIONING SMART CONTRACT EXAMPLE*******************
+pragma solidity ^0.4.18;
+
+contract timeCapsule {
+   
+    mapping (address => string) message;
+    mapping (address => uint) order;
+    
+    function setEntry(string message) public {
+       message[msg.sender] = message;
+       order[msg.sender] = now;
+    }
+
+    function getEntry(address account) public view returns(string, uint) {
+      return (message[account], order[account]);
+    }
+
+}
+**************************************************************************/
