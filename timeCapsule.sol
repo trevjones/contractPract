41ede2256a1ff7d1435665b58f2a7995fd14e45b
Trevor Jones <38113711+trevjones@users.noreply.github.com>
2018-09-10 12:42:30 -0700

    attempting own smart contract to fit needs of ETA

---
+++ timeCapsule.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.18;
+
+contract timeCapsule {
+   
+    mapping (address => string) message;
+    mapping (address => uint) order;
+    
+    function setEntry(string message) public {
+       message[msg.sender] = message;
+       order[msg.sender] = now;
+    }
+
+    function getEntry(address account) public view returns(string, uint) {
+      return (message[account], order[account]);
+    }
+
+}
