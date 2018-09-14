fd3e5cfd3ab00af87e55e988b6d0d78d67294b7d
Trevor Jones <38113711+trevjones@users.noreply.github.com>
2018-09-14 12:27:49 -0700

    Update timeCapsule.sol

--- timeCapsule.sol
+++ timeCapsule.sol
@@ -4,14 +4,16 @@ contract timeCapsule {
    
     mapping (address => string) message;
     mapping (address => uint) order;
+    uint entryOrder = 0; //establish first entry : "000"
     
     function setEntry(string entry) public {
        message[msg.sender] = entry;
        order[msg.sender] = entryOrder;
+       entryOrder++; //increment entry #
     }
 
     function getEntry(address account) public view returns(string, uint) {
-      return (message[account], entryOrder[account]);
+      return (message[account], order[account]);
     }
 
 }
