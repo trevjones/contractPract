fc0bbca09ca8fd4d1baf2372102f872015c80467
Trevor Jones <38113711+trevjones@users.noreply.github.com>
2018-09-14 12:21:05 -0700

    Update timeCapsule.sol

--- timeCapsule.sol
+++ timeCapsule.sol
@@ -5,33 +5,13 @@ contract timeCapsule {
     mapping (address => string) message;
     mapping (address => uint) order;
     
-    function setEntry(string message) public {
-       message[msg.sender] = message;
-       order[msg.sender] = now;
+    function setEntry(string entry) public {
+       message[msg.sender] = entry;
+       order[msg.sender] = entryOrder;
     }
 
     function getEntry(address account) public view returns(string, uint) {
-      return (message[account], order[account]);
+      return (message[account], entryOrder[account]);
     }
 
 }
-
-/******ORIGINAL SNIPPET FUNCTIONING SMART CONTRACT EXAMPLE*******************
-pragma solidity ^0.4.18;
-
-contract MyDetails {
-   
-    mapping (address => string) ipfsHashes;
-    mapping (address => uint) timestamp;
-    
-    function setHash(string ipfsHash) public {
-       ipfsHashes[msg.sender] = ipfsHash;
-       timestamp[msg.sender] = now;
-    }
-
-    function getHash(address account) public view returns(string, uint) {
-      return (ipfsHashes[account], timestamp[account]);
-    }
-
-}
-**************************************************************************/
