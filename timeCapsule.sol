a9b42b19b0fefafdddf52c57bf09517fb1dc3801
Trevor Jones <38113711+trevjones@users.noreply.github.com>
2018-09-14 13:37:08 -0700

    Update timeCapsule.sol

--- timeCapsule.sol
+++ timeCapsule.sol
@@ -2,18 +2,19 @@ pragma solidity ^0.4.18;
 
 contract timeCapsule {
    
+    address capsule;
     mapping (address => string) message;
     mapping (address => uint) order;
-    uint entryOrder = 0; //establish first entry : "000"
+    uint entryOrder = 0; //initialize entries => "000"
     
     function setEntry(string entry) public {
-       message[msg.sender] = entry;
-       order[msg.sender] = entryOrder;
+       message[capsule] = entry;
+       order[capsule] = entryOrder;
        entryOrder++; //increment entry #
     }
 
-    function getEntry(address account) public view returns(string, uint) {
-      return (message[account], order[account]);
+    function getEntry() public view returns(string, uint) {
+      return (message[capsule], order[capsule]);
     }
-
+    
 }
